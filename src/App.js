06364c3ad0d7434d9c0507ab0fc496d64cd42cd8
Christian Montoya <chrimonlo@gmail.com>
2022-01-20 11:55:29 -0500

    pokémon API complete project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useEffect, useState } from 'react';
+import Card from './views/Pokemon/components/Card';
 
 function App() {
+  const [listPokemon, setListPokemon] = useState([]);
+  useEffect(() => {
+    const consumeApi = async () => {
+      fetch("https://pokeapi.co/api/v2/pokemon?limit=200").then(result => result.json()).then(data => {
+        data.results.forEach(value => {
+          fetch(value.url).then(result2 => result2.json()).then(data2 => {
+            setListPokemon(prev => [...prev.sort((a,b) => a.id - b.id), {
+              id: data2.id,
+              name: data2.name,
+              img: data2.sprites.front_default,
+              type: data2.types[0].type.name
+            }]);
+          });
+        });
+      })
+    }
+    consumeApi();
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className='title'>PokeDex</h1>
+      <div className="cards">
+        {listPokemon?.map(val => (<Card key={val.id} data={val}/>))}
+      </div>
+      
     </div>
   );
+  
 }
 
 export default App;
---
+++ src/views/Pokemon/components/Card/index.jsx
@@ -0,0 +1,30 @@
+import "./style.css";
+
+export default function Card(props) {
+  const colors = {
+    fire: "#FF4141",
+    grass: "#5DF64E",
+    electric: "#F3EA28",
+    water: "#51C7FF",
+    ground: "#F19D49",
+    rock: "#D7D739",
+    fairy: "#E237FF",
+    poison: "#39DA5A",
+    bug: "#FBBF6A",
+    dragon: "#3554CB",
+    psychic: "#D1D660",
+    flying: "#F1E9E9",
+    fighting: "#E9DEC8",
+    normal: "#DBD9D9",
+  };
+  return (
+    <div className="card" style={{ backgroundColor: colors[props.data.type] }}>
+      <div className="imgContainer">
+        <img src={props.data.img} alt={props.data.name} />
+      </div>
+      <p>{props.data.id}</p>
+      <p>{props.data.name}</p>
+      <p>{props.data.type}</p>
+    </div>
+  );
+}
