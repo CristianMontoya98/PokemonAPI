b763f7fad09aee4b5276a67e232040339ed8bcf2
Christian Montoya <chrimonlo@gmail.com>
2022-01-25 14:19:36 -0500

    Actualization styles

--- src/views/Pokemon/components/Card/index.jsx
+++ src/views/Pokemon/components/Card/index.jsx
@@ -1,4 +1,4 @@
-import "./style.css";
+import styles from"./style.module.css";
 
 export default function Card(props) {
   const colors = {
@@ -7,20 +7,23 @@ export default function Card(props) {
     electric: "#F3EA28",
     water: "#51C7FF",
     ground: "#F19D49",
-    rock: "#D7D739",
+    rock: "#736326",
     fairy: "#E237FF",
     poison: "#39DA5A",
     bug: "#FBBF6A",
     dragon: "#3554CB",
-    psychic: "#D1D660",
+    psychic: "#288766",
     flying: "#F1E9E9",
     fighting: "#E9DEC8",
     normal: "#DBD9D9",
+    ghost: "#9B9797",
+    ice: "#A1CBC7",
+    dark: "#243A3A"
   };
   return (
-    <div className="card" style={{ backgroundColor: colors[props.data.type] }}>
-      <p className="card__id">{props.data.id}</p>
-      <div className="imgContainer">
+    <div className={styles.card} style={{ backgroundColor: colors[props.data.type] }}>
+      <p className={styles.card__id}>{props.data.id}</p>
+      <div className={styles.imgContainer}>
         <img src={props.data.img} alt={props.data.name} />
       </div>
       <p>{props.data.name}</p>
